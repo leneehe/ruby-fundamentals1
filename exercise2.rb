22dd254a3082deaaf2ce62704d35c4e7cea835d9
Lena <fatmeow@gmail.com>
2017-10-30 23:12:24 -0400

    Complete exercise 2

---
+++ exercise2.rb
@@ -0,0 +1,13 @@
+## Exercise 2
+
+#How would you calculate a good tip for a 55 dollar meal?
+puts "1. A good tip for $55 meal is times by 15% which is $#{55 * 0.15}."
+
+#add string and interger with + operator, convert integer to string
+puts "2. Hello world! " + 123.to_s
+
+#the result of 45628 multiplied by 7839 in a sentence by using string interpolation.
+puts "3. Result by string interpolation: #{45628 * 7839}"
+
+# (true && false =  false || true) = true
+puts "4. " + ((10 < 20 && 30 < 20) || !(10 == 11)).to_s
