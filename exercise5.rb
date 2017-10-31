443d36255446806dfb366e8e53b5f0ccb8559300
Lena <fatmeow@gmail.com>
2017-10-31 01:13:18 -0400

    Complete exercise 6.2

--- exercise5.rb
+++ exercise5.rb
@@ -1,15 +1,20 @@
 distance = 0
 travel = ""
+energy = 50
 
 until travel == "go home"
   puts "Would you like to walk or run? or go home?"
   travel = gets.chomp
   if travel == "walk"
     distance += 1
-    puts "Distance from home is #{distance}km."
-  elsif travel == "run"
+    energy += 5
+    puts "Distance from home is #{distance}km. Your energy is #{energy}"
+  elsif travel == "run" && energy > 0
     distance += 5
-    puts "Distance from home is #{distance}km."
+    energy -= 5
+    puts "Distance from home is #{distance}km. Your energy is #{energy}"
+  elsif travel == "run" && energy <= 0
+    puts "Your energy is #{energy}. You cannot run!"
   elsif travel != "go home"
     puts "this command does not exist!"
   end
