6861a6f0a8a0b7804a59a7d77a8f13f16a429293
Lena <fatmeow@gmail.com>
2017-10-30 23:44:46 -0400

    Use gets.chomp and convert string to integer together.

--- exercise3.rb
+++ exercise3.rb
@@ -6,3 +6,7 @@ puts "Hello, #{user_name}"
 puts "What is your name again?"
 user_name = gets.chomp
 puts "Hello again, #{user_name}"
+
+puts "How old are you?"
+age = gets.chomp.to_i
+puts "You are born in the year of #{(2017 - age)}."
